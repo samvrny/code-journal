1daae17b9bf719e9603efe0b07b9e89928fa465c
samvrny <samvrny@gmail.com>
2022-12-20 12:54:58 -0600

    worked more on the entries page. Got it looking almost where I want it to be for the MVP

--- src/components/Entries/index.js
+++ src/components/Entries/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import WebFont from "webfontloader";
 
 function Entries() {
     const jspicture = require('../../assets/images/javascript.png');
@@ -9,10 +10,20 @@ function Entries() {
     const miscpicture = require('../../assets/images/misc.jpg');
     const apipicture = require('../../assets/images/api.jpg');
 
+    const loadWebFont = async () => {
+        WebFont.load({
+            google: {
+                families: ['Bungee Shade', 'Luckiest Guy']
+            }
+        });
+    }
+
+    loadWebFont()
+
     return (
         <section className='entries-body'>
-            <section>
-                <h1>Entries content page!</h1>
+            <section className='entries-opening'>
+                <h1 style={{ fontFamily: 'Bungee Shade' }}>Entries content page!</h1>
                 <p>So many things to put in here! Javascript, CSS, HTML, NodeJS,
                     REST APIs, Fetch, MongoDB, MySQL, Express, so on and so forth!
                     Algorithms, and more. hello I just need more words to fill this out
@@ -20,28 +31,52 @@ function Entries() {
                 </p>
             </section>
             <section className='flex around wrap entry-links'>
-                <div className='link-a javascript'>
+                <div className='link-a around javascript'>
                     {/* <img src={jspicture} /> */}
                     <Link className='explore-button flex center' to='/javascript'>Explore</Link>
+                    <div className='section-about'>
+                        <h4>Javascript</h4>
+                        <p>Hello World</p>
+                    </div>
                 </div>
-                <div className='link-b css'>
+                <div className='link-b around css'>
                     {/* <img src={csspicture} /> */}
+                    <div className='section-about'>
+                        <h4>CSS</h4>
+                        <p>Hello World</p>
+                    </div>
                     <Link className='explore-button flex center' to='/css'>Explore</Link>
                 </div>
-                <div className='link-a html'>
+                <div className='link-a around html'>
                     {/* <img src={htmlpicture} /> */}
                     <Link className='explore-button flex center' to='/html'>Explore</Link>
+                    <div className='section-about'>
+                        <h4>HTML</h4>
+                        <p>Hello World!</p>
+                    </div>
                 </div>
-                <div className='link-b algorithm'>
+                <div className='link-b around algorithm'>
                     {/* <img src={algopicture} /> */}
+                    <div className='section-about'>
+                        <h4>Algorithms</h4>
+                        <p>Hello World</p>
+                    </div>
                     <Link className='explore-button flex center' to='/algorithms'>Explore</Link>
                 </div>
-                <div className='link-a misc'>
+                <div className='link-a around misc'>
                     {/* <img src={miscpicture} /> */}
                     <Link className='explore-button flex center' to='/misc'>Explore</Link>
+                    <div className='section-about'>
+                        <h4>Misc</h4>
+                        <p>Hello World</p>
+                    </div>
                 </div>
-                <div className='link-b api'>
+                <div className='link-b around api'>
                     {/* <img src={apipicture} /> */}
+                    <div className='section-about'>
+                        <h4>Apis</h4>
+                        <p>Hello World</p>
+                    </div>
                     <Link className='explore-button flex center' to='/apis'>Explore</Link>
                 </div>
             </section>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+import WebFont from "webfontloader";
 
 function Header() {
 
+    const loadWebFont = async () => {
+        WebFont.load({
+            google: {
+                families: ['Bungee Inline']
+            }
+        });
+    }
+
+    loadWebFont()
+
     return (
         <header className='flex between'>
-            <h1>Code Handbook</h1>
+            <h1 style={{fontFamily: 'Bungee Inline'}}>Code Handbook</h1>
             <nav>
-                <div className='flex around'>
+                <div className='flex around' style={{fontFamily: 'Bungee Inline'}}>
                     <Link className='nav-link' to='/'>Home</Link>
                     <Link className='nav-link' to='/entries'>Entries</Link>
                     <Link className='nav-link' to='/contact'>Contact</Link>
