a334c1ffe2d3d1e2e34a0284a0bff70b4c04e9ec
samvrny <samvrny@gmail.com>
2022-12-19 20:35:25 -0600

    played with the entries page a lot. Working on what the page will ultimately look like

--- src/components/Entries/index.js
+++ src/components/Entries/index.js
@@ -2,21 +2,48 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Entries() {
+    const jspicture = require('../../assets/images/javascript.png');
+    const htmlpicture = require('../../assets/images/html.png');
+    const csspicture = require('../../assets/images/css.png');
+    const algopicture = require('../../assets/images/algorithm.jpeg');
+    const miscpicture = require('../../assets/images/misc.jpg');
+    const apipicture = require('../../assets/images/api.jpg');
+
     return (
         <section className='entries-body'>
-            <h1>Entries content page!</h1>
-            <p>So many things to put in here! Javascript, CSS, HTML, NodeJS,
-                REST APIs, Fetch, MongoDB, MySQL, Express, so on and so forth!
-                Algorithms, and more. hello I just need more words to fill this out
-                so I can see what my flexbox is actually doing. Ok it appears to be working!!
-            </p>
+            <section>
+                <h1>Entries content page!</h1>
+                <p>So many things to put in here! Javascript, CSS, HTML, NodeJS,
+                    REST APIs, Fetch, MongoDB, MySQL, Express, so on and so forth!
+                    Algorithms, and more. hello I just need more words to fill this out
+                    so I can see what my flexbox is actually doing. Ok it appears to be working!!
+                </p>
+            </section>
             <section className='flex around wrap entry-links'>
-                <Link to='/javascript'>Javascript</Link>
-                <Link to='/css'>CSS</Link>
-                <Link to='/html'>HTML</Link>
-                <Link to='/algorithms'>Algorithms</Link>
-                <Link to='/misc'>Misc</Link>
-                <Link to='/apis'>APIs</Link>
+                <div className='link-a'>
+                    {/* <img src={jspicture} /> */}
+                    <Link className='explore-button flex center' to='/javascript'>Javascript</Link>
+                </div>
+                <div className='link-b'>
+                    {/* <img src={csspicture} /> */}
+                    <Link className='explore-button flex center' to='/css'>CSS</Link>
+                </div>
+                <div className='link-a'>
+                    {/* <img src={htmlpicture} /> */}
+                    <Link className='explore-button flex center' to='/html'>HTML</Link>
+                </div>
+                <div className='link-b'>
+                    {/* <img src={algopicture} /> */}
+                    <Link className='explore-button flex center' to='/algorithms'>Algorithms</Link>
+                </div>
+                <div className='link-a'>
+                    {/* <img src={miscpicture} /> */}
+                    <Link className='explore-button flex center' to='/misc'>Misc</Link>
+                </div>
+                <div className='link-b'>
+                    {/* <img src={apipicture} /> */}
+                    <Link className='explore-button flex center' to='/apis'>APIs</Link>
+                </div>
             </section>
         </section>
     )
